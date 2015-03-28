406cbfef9cdc83ef2cb9b0205d5a95b8a2fa4dc6
Jeremy Bader <jbader@conncoll.edu>
2015-03-28 14:53:47 -0500

    Added symbol manager script and capsule

---
+++ TwerkProject/Assets/Scripts/SymbolManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SymbolManager : MonoBehaviour {
+	public GameObject symbol;
+	public Vector3 spawnValues;
+	public float spawnWait;
+	public float startWait;
+
+	void Start ()
+	{
+		DropSymbol();
+	}
+	
+	IEnumerator DropSymbol ()
+	{
+		yield return new WaitForSeconds (startWait);
+		while (true)
+		{
+			Vector3 spawnPosition = new Vector3(spawnValues.x, spawnValues.y, spawnValues.z);
+			//Instantiate (symbol, spawnPosition);
+			yield return new WaitForSeconds (spawnWait);
+		}
+	}
+}
