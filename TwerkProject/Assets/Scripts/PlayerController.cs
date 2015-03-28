aff8fd346678d75434f7f559557bd73e98145392
jimunoz1990 <jimunoz1990@gmail.com>
2015-03-28 16:05:37 -0500

    Disables the random scoring system and capsule responds on input now

--- TwerkProject/Assets/Scripts/PlayerController.cs
+++ TwerkProject/Assets/Scripts/PlayerController.cs
@@ -9,22 +9,34 @@ public class PlayerController : MonoBehaviour
 	void Update ()
 	{
 		Animator animator = this.GetComponentInChildren<Animator> ();
-
-		if (Input.GetButton ("Left")) {
-			animator.SetBool ("Moving", true);
-			animator.transform.localScale = new Vector3 (-1, 1, 1);
+		if (Input.GetButton ("Up")) {
+			animator.SetInteger ("transitionInt", 1);
+			Debug.Log ("UP was pressed", gameObject);
+			Invoke ("returnToIdle", 2);
+			//animator.transform.localScale = new Vector3 (1, 1, 1);
 		}
 		if (Input.GetButton ("Right")) {
-			animator.SetBool ("Moving", true);
-			animator.transform.localScale = new Vector3 (1, 1, 1);
-		}
-		if (!Input.GetButton ("Up")) {
-			animator.SetBool ("Moving", true);
-			animator.transform.localScale = new Vector3 (1, 1, 1);
+			animator.SetInteger ("transitionInt", 2);
+			Debug.Log ("RIGHT was pressed", gameObject);
+			Invoke ("returnToIdle", 2);
+			//animator.transform.localScale = new Vector3 (1, 1, 1);
 		}
 		if (Input.GetButton ("Down")) {
-			animator.SetBool ("Moving", true);
-			animator.transform.localScale = new Vector3 (1, 1, 1);
+			animator.SetInteger ("transitionInt", 3);
+			Debug.Log ("DOWN was pressed", gameObject);
+			Invoke ("returnToIdle", 2);
+			//animator.transform.localScale = new Vector3 (1, 1, 1);
+		}		
+		if (Input.GetButton ("Left")) {
+			animator.SetInteger ("transitionInt", 4);
+			Debug.Log ("LEFT was pressed", gameObject);
+			Invoke ("returnToIdle", 2);
+			//animator.transform.localScale = new Vector3 (-1, 1, 1);
 		}
+	}
+
+	void returnToIdle () {
+		Animator animator = this.GetComponentInChildren<Animator> ();
+		animator.SetInteger ("transitionInt", 0);
 	}
 }
