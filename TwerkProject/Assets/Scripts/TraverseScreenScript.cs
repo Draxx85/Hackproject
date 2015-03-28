fb9a52816e1d60242494e26e22d758e95161013c
jimunoz1990 <jimunoz1990@gmail.com>
2015-03-28 00:07:08 -0500

    Merge

---
+++ TwerkProject/Assets/Scripts/TraverseScreenScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TraverseScreenScript : MonoBehaviour {
+
+    public string sceneToLoad;
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(500, 450, 200, 50), "Click Here or Press Start"))
+        {
+            print("Load Scene: " + sceneToLoad);
+            Application.LoadLevel(sceneToLoad); 
+        }
+    }
+
+    void Start()
+    {
+        print("Current Scene: " + Application.loadedLevelName);
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Submit"))
+        {
+            print("Load Scene: " + sceneToLoad);
+            Application.LoadLevel(sceneToLoad); 
+        }
+    }
+}
