baea05a0e9645112cecdb788d01848173f1f762d
jimunoz1990 <jimunoz1990@gmail.com>
2015-03-28 12:47:24 -0500

    enables the idle stage in between animations

--- TwerkProject/Assets/Scripts/TwerkAnim.cs
+++ TwerkProject/Assets/Scripts/TwerkAnim.cs
@@ -4,27 +4,41 @@ using System.Collections;
 public class TwerkAnim : MonoBehaviour {
 
 	/* TwerkAnim class will be used to determine which Twerk animation to perform
-	 * 1 - Miss
-	 * 2 - Poor
-	 * 3 - Great
-	 * 4 - Perfect
-	 * 
+	 * -1 - Idle
+	 *  1 - Miss
+	 *  2 - Poor
+	 *  3 - Great
+	 *  4 - Perfect
 	 */
 
 	Animator animator;
 
 	int transitionInt = -1;
 	int streak = 0;
+	float animTimer;
 
 	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator>();
+		animTimer = 0;
 		//this will be removed and determineAnimation will be called by Jeremy's function that finds the accuracy of a move
 		InvokeRepeating("determineRandomAnimation", 0.0f, 4.0f);
 	}
 
+	void Update() {
+		animTimer += Time.deltaTime;
+
+		if (animTimer > 2.1f && transitionInt >= 0) {
+			transitionInt = -1;
+			animator.SetInteger ("transitionInt", transitionInt);
+			Debug.Log ("Transition Reset: " + transitionInt.ToString (), gameObject);
+			Debug.Log ("Transition Timer: " + animTimer.ToString (), gameObject);
+		}
+	}
+
 	public void determineRandomAnimation(){
 		transitionInt = Random.Range(1, 4);
+		animTimer = 0;
 		
 		Debug.Log ("Transition: " + transitionInt.ToString (), gameObject);
 		animator.SetInteger ("transitionInt", transitionInt);
