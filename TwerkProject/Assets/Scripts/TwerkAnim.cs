996f2e86246a6d4179daaa4c16f24f1709215ea3
jimunoz1990 <jimunoz1990@gmail.com>
2015-03-27 23:49:37 -0500

    Adds scripts folder

---
+++ TwerkProject/Assets/Scripts/TwerkAnim.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TwerkAnim : MonoBehaviour {
+
+	/*  TwerkAnim class will be used to determine which Twerk animation to perform
+	 * 0 - Terrible
+	 * 1 - Poor
+	 * 2 - Okay
+	 * 3 - Good
+	 * 
+	 */
+
+
+// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
