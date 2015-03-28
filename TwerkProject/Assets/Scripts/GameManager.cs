153309b04a68d380d370435d3e6b42b4caf205a9
kkdaman <kkdaman@hotmail.com>
2015-03-28 02:55:15 -0500

    Added update Score to game manager

--- TwerkProject/Assets/Scripts/GameManager.cs
+++ TwerkProject/Assets/Scripts/GameManager.cs
@@ -15,67 +15,31 @@ public class GameManager : MonoBehaviour {
 			instance = this;
 		}
 	}
-
-	public Transform orbPrefab;
-	public GUIText scorePrefab;
-    public Transform currentOrb;
-    public string sceneToLoad;
-	private float scoreSpacing = 0.1f;
-	private Dictionary<PlayerController, int> playerScores = new Dictionary<PlayerController,int>();
-	private Dictionary<PlayerController, GUIText> scoreBoard = new Dictionary<PlayerController, GUIText>();
-	public int winningScore = 5;
-
-	public float minRotationInterval = 5;
-	public float maxRotationInterval = 20;
-	private float nextRotationTime;
-    private int previousSpawnLocation = int.MaxValue;
-    private GameObject[] orbSpawns;
+	
+	public bool hasNewScore = true;
+	public int scoreDelta = 5;
 
 	// Use this for initialization
     void Start()
-    {
+	{
+		ScoreManager.score = 1;
+		MeterManager.fill = 10;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		ScoreManager.score = 1;
-		MeterManager.fill = 50;
-//		GameObject.FindObjectsOfType<PlayerController>()
-//		foreach (PlayerController playerController in playerScores.Keys) {
-//			scoreBoard[playerController].text = playerController.tag + ": " + playerScores[playerController];
-//		}
+		scoreDelta = 5;
 
-//        if (getCurrentWinner() != null || (Input.GetButton("Cancel") && !string.IsNullOrEmpty(sceneToLoad)))
-//        {
-//            foreach (PlayerController playerController in playerScores.Keys)
-//            {
-//                PlayerPrefs.SetInt(playerController.tag, playerScores[playerController]);
-//            }
-//
-//            print("Load Scene: " + sceneToLoad);
-//            Application.LoadLevel(sceneToLoad); 
-//        }
+		if (hasNewScore) {
+			ScoreManager.score += scoreDelta;
+			MeterManager.fill += scoreDelta;
+		}
+
+		hasNewScore = false;
 	}
 
-    public Dictionary<PlayerController, int> GetScores()
-    {
-        return playerScores;
-    }
-
-//	public void ReportScore(PlayerController playerController) {
-//		if (playerScores.ContainsKey(playerController)) {
-//			playerScores[playerController] += 1;
-//		} else {
-//			playerScores.Add(playerController, 1);
-//			scoreBoard.Add(playerController, ((GameObject)GameObject.Instantiate(scorePrefab)).GetComponent<GUIText>());
-//		}
-//	}
-//	public void ReportScore(PlayerController playerController, int points) {
-//		if (playerScores.ContainsKey(playerController)) {
-//			playerScores[playerController] += points;
-//		} else {
-//			playerScores.Add(playerController, points);
-//			scoreBoard.Add(playerController, ((GameObject)GameObject.Instantiate(scorePrefab.gameObject, new Vector3(0.05f, 0.95f - scoreBoard.Count * scoreSpacing, 0), Quaternion.identity)).GetComponent<GUIText>());
-//		}
-//	}
+	public void UpdateScore(int delta){
+		hasNewScore = true;
+		scoreDelta = delta;
+	}
 }
