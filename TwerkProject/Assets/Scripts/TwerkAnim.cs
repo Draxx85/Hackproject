7a19f80edeab840228eab7c631638102633d6b71
jimunoz1990 <jimunoz1990@gmail.com>
2015-03-28 03:30:50 -0500

    Sets up score update and twerk anim to receive and put out correct data

--- TwerkProject/Assets/Scripts/TwerkAnim.cs
+++ TwerkProject/Assets/Scripts/TwerkAnim.cs
@@ -8,32 +8,46 @@ public class TwerkAnim : MonoBehaviour {
 	 * 2 - Poor
 	 * 3 - Great
 	 * 4 - Perfect
+	 * 
 	 */
 
 	Animator animator;
-	
-	float animTime = 0.0f;
-	int transitionInt = -1;
 
-// Use this for initialization
+	int transitionInt = -1;
+	int streak = 0;
+
+	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator>();
-		InvokeRepeating("determineAnimation", 0.0f, 4.0f);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		animTime += Time.deltaTime;
-
-		if (animTime > 1.1f)
-			animTime = 0; 
+		//this will be removed and determineAnimation will be called by Jeremy's function that finds the accuracy of a move
+		InvokeRepeating("determineRandomAnimation", 0.0f, 4.0f);
 	}
 
-	public void determineAnimation(){
+	public void determineRandomAnimation(){
 		transitionInt = Random.Range(1, 4);
-		animTime = 0;
-
+		
 		Debug.Log ("Transition: " + transitionInt.ToString (), gameObject);
 		animator.SetInteger ("transitionInt", transitionInt);
+	}	
+
+	public void determineAnimation(int score){
+		if (score < 10) {
+			streak = 0;
+			transitionInt = 1;
+		} else if (score < 50) {
+			streak++;
+			transitionInt = 2;
+		} else if (score < 80) {
+			streak++;
+			transitionInt = 3;
+		} else {
+			streak++;
+			transitionInt = 4;
+		}
+		
+		Debug.Log ("Transition: " + transitionInt.ToString (), gameObject);
+		animator.SetInteger ("transitionInt", transitionInt);
+
+		ScoreUpdate.updateScore (score, streak);
 	}
 }
