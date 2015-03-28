8dc059936050b626e9e415e3339d23c785cd7f92
jimunoz1990 <jimunoz1990@gmail.com>
2015-03-28 00:45:12 -0500

    state transition WIP

--- TwerkProject/Assets/Scripts/TwerkAnim.cs
+++ TwerkProject/Assets/Scripts/TwerkAnim.cs
@@ -3,22 +3,49 @@ using System.Collections;
 
 public class TwerkAnim : MonoBehaviour {
 
-	/*  TwerkAnim class will be used to determine which Twerk animation to perform
-	 * 0 - Terrible
-	 * 1 - Poor
-	 * 2 - Okay
-	 * 3 - Good
-	 * 
+	/* TwerkAnim class will be used to determine which Twerk animation to perform
+	 * 1 - Miss
+	 * 2 - Poor
+	 * 3 - Great
+	 * 4 - Perfect
 	 */
+	private Color playerColor = new Color();
 
+
+	float animTime = 0.0f;
+	int transitionInt = -1;
 
 // Use this for initialization
 	void Start () {
-	
+
+		InvokeRepeating("determineAnimation", 0.0f, 4.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		animTime += Time.deltaTime;
+
+		if (animTime > 1.1f)
+			animTime = 0; 
+	}
+
+	public void determineAnimation(){
+		transitionInt = Random.Range(1, 4);
+		animTime = 0;
+
+		switch (transitionInt) {
+		case 1:
+			// put the color changes here
+			break;
+		case 2:
+			// put the color changes here
+			break;
+		case 3:
+			// put the color changes here
+			break;
+		case 4:
+			//stuff
+			break;
+		}
 	}
 }
