d9f58a75b7663a89631b56ab587470fe37430e61
kkdaman <kkdaman@hotmail.com>
2015-03-28 15:41:54 -0500

    Adding a countdown timer for the stage

---
+++ TwerkProject/Assets/Scripts/RemainingTimeManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class RemainingTimeManager : MonoBehaviour
+{
+	private Text text;
+	private float startTime = 0.0f;
+	private float countDownSeconds = 0.0f;
+	
+	public float startCountdownTime = 90.0f;
+	
+	void Awake ()
+	{
+		text = GetComponent <Text> ();
+		startTime = Time.time;
+		countDownSeconds = startCountdownTime;
+	}
+	
+	void Update ()
+	{
+		// Change the remaining time
+		if (Time.time - startTime >= 1) {
+			startTime = Time.time;
+			countDownSeconds--;
+		}
+
+		text.text = "Time Remaining: " + countDownSeconds;
+	}
+}
