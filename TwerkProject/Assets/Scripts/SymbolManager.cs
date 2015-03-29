861a9367b7d648d1df7c052ea79dacbc59633cba
JBader89 <JBader@conncoll.edu>
2015-03-28 20:41:32 -0500

    Added randomness of arrows on each start

--- TwerkProject/Assets/Scripts/SymbolManager.cs
+++ TwerkProject/Assets/Scripts/SymbolManager.cs
@@ -2,13 +2,42 @@
 using System.Collections;
 
 public class SymbolManager : MonoBehaviour {
-	public GameObject Symbol;
+	public GameObject MainSymbol;
+	public GameObject UpSymbol;
+	public GameObject DownSymbol;
+	public GameObject LeftSymbol;
+	public GameObject RightSymbol;
+
 	public Vector3 spawnValues;
 	public float spawnWait;
 	public float startWait;
 	public float fallSpeed;
-
+	
 	void Start() {
+		UpSymbol.SetActive (false);
+		DownSymbol.SetActive (false);
+		LeftSymbol.SetActive (false);
+		RightSymbol.SetActive (false);
+
+		switch (Random.Range(0,4))
+		{
+		case 0:
+			UpSymbol.SetActive (true);
+			break;
+		case 1:
+			DownSymbol.SetActive (true);
+			break;
+		case 2:
+			LeftSymbol.SetActive (true);
+			break;
+		case 3:
+			RightSymbol.SetActive (true);
+			break;
+		default:
+			Debug.Log ("Shouldn't reach here.");
+			break;
+		}
+		
 		StartCoroutine (makeSymbol ());
 	}
 
@@ -17,14 +46,13 @@ public class SymbolManager : MonoBehaviour {
 		yield return new WaitForSeconds (startWait);
 		while (true)
 		{
-			Vector3 spawnPosition = new Vector3 (7, 7, 0);
-			Instantiate (Symbol, spawnPosition, Quaternion.identity);
+			Vector3 spawnPosition = new Vector3 (300, 275, 0);
+			Instantiate (MainSymbol, spawnPosition, Quaternion.identity);
 			yield return new WaitForSeconds (spawnWait);
 		}
 	}
 
 	void Update() {
 		transform.Translate(spawnValues.x, spawnValues.y - fallSpeed, spawnValues.z);
-		
 	}
 }
