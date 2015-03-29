2e98e3670abeda80908aba62ad78b3ea83cab59d
JBader89 <JBader@conncoll.edu>
2015-03-29 01:25:14 -0500

    Added randomization to arrow choice

--- TwerkProject/Assets/Scripts/SymbolManager.cs
+++ TwerkProject/Assets/Scripts/SymbolManager.cs
@@ -3,15 +3,42 @@ using System.Collections;
 
 public class SymbolManager : MonoBehaviour {
 	public GameObject Symbol;
+
 	public Vector3 spawnValues;
 	public float spawnWait;
 	public float startWait;
 	public float fallSpeed;
 
+	private Sprite currentSymbol;
+	private SpriteRenderer myRenderer;
+	public Sprite leftSprite;
+	public Sprite rightSprite;
+	public Sprite upSprite;
+	public Sprite downSprite;
+
+	public int actuState = 3;
+
 	void Start() {
 		StartCoroutine (makeSymbol ());
-	}
+		myRenderer = Symbol.GetComponent<SpriteRenderer>();
 
+		switch(Random.Range (0,4) )
+		{
+		case 0:
+			myRenderer.sprite = leftSprite;
+			break;
+		case 1:
+			myRenderer.sprite = rightSprite;
+			break;
+		case 2:
+			myRenderer.sprite = upSprite;
+			break;
+		case 3:
+			myRenderer.sprite = downSprite;
+			break;
+		}
+	}
+	
 	IEnumerator makeSymbol ()
 	{
 		yield return new WaitForSeconds (startWait);
