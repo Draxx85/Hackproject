09c9d4a612a22eb2fd742ceb6aaa7150aab234d5
JBader89 <JBader@conncoll.edu>
2015-03-29 00:53:13 -0500

    Checking in current version of drop animation

--- TwerkProject/Assets/Scripts/SymbolManager.cs
+++ TwerkProject/Assets/Scripts/SymbolManager.cs
@@ -2,42 +2,13 @@
 using System.Collections;
 
 public class SymbolManager : MonoBehaviour {
-	public GameObject MainSymbol;
-	public GameObject UpSymbol;
-	public GameObject DownSymbol;
-	public GameObject LeftSymbol;
-	public GameObject RightSymbol;
-
+	public GameObject Symbol;
 	public Vector3 spawnValues;
 	public float spawnWait;
 	public float startWait;
 	public float fallSpeed;
-	
-	void Start() {
-		UpSymbol.SetActive (false);
-		DownSymbol.SetActive (false);
-		LeftSymbol.SetActive (false);
-		RightSymbol.SetActive (false);
 
-		switch (Random.Range(0,4))
-		{
-		case 0:
-			UpSymbol.SetActive (true);
-			break;
-		case 1:
-			DownSymbol.SetActive (true);
-			break;
-		case 2:
-			LeftSymbol.SetActive (true);
-			break;
-		case 3:
-			RightSymbol.SetActive (true);
-			break;
-		default:
-			Debug.Log ("Shouldn't reach here.");
-			break;
-		}
-		
+	void Start() {
 		StartCoroutine (makeSymbol ());
 	}
 
@@ -46,13 +17,14 @@ public class SymbolManager : MonoBehaviour {
 		yield return new WaitForSeconds (startWait);
 		while (true)
 		{
-			Vector3 spawnPosition = new Vector3 (-423, -211, 0);
-			Instantiate (MainSymbol, spawnPosition, Quaternion.identity);
+			Vector3 spawnPosition = new Vector3 (7, 7, 0);
+			Instantiate (Symbol, spawnPosition, Quaternion.identity);
 			yield return new WaitForSeconds (spawnWait);
 		}
 	}
 
 	void Update() {
 		transform.Translate(spawnValues.x, spawnValues.y - fallSpeed, spawnValues.z);
+		
 	}
 }
