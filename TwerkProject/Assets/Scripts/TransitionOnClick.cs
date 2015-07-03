02916621824b08d274a58e2f564b5f2f203be77f
kkdaman <kkdaman@hotmail.com>
2015-07-03 13:27:40 -0500

    New script for transitioning scenes on a button click Play button now animates and transitions to level 1

---
+++ TwerkProject/Assets/Scripts/TransitionOnClick.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransitionOnClick : MonoBehaviour {
+
+	public void TranstitionTo(string sceneToLoad){
+		Debug.Log ("TransitionOnClick");
+
+		if (!string.IsNullOrEmpty (sceneToLoad)) {
+			Debug.Log ("Load Scene: " + sceneToLoad);
+			Application.LoadLevel (sceneToLoad);
+		} else {
+			Debug.LogWarning ("No scene to load");
+		}
+	}
+}
