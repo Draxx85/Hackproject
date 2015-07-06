425e9d73258014a4f665e2ef191f8212e7f3be93
kkdaman <kkdaman@hotmail.com>
2015-07-05 20:38:44 -0500

    New an improved menu screen

---
+++ TwerkProject/Assets/Scripts/SimpleSpriteToggleTimer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SimpleSpriteToggleTimer : MonoBehaviour {
+	
+	private float lastToggleTime = 0.0f;
+
+	public float timeInterval = 3.0f;
+	public Sprite spriteA;
+	public Sprite spriteB;
+
+	// Use this for initialization
+	void Start () {
+		
+		lastToggleTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Time.time - lastToggleTime > timeInterval) {
+			lastToggleTime = Time.time;
+			Image currentSprite = gameObject.GetComponent<Image> ();
+			if(currentSprite.sprite.name == spriteA.name){
+				currentSprite.sprite = spriteB;
+			}else{
+				currentSprite.sprite = spriteA;
+			}
+		}
+	}
+}
